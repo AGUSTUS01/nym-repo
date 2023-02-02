01ff98a9fa32c731b1dffc7f3a087c674ceea229
AGUSTUS <112334233+AGUSTUS01@users.noreply.github.com>
2023-02-02 21:22:52 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,52 @@
+console.log('loops')
+console.log('for loops, while loops, bla bla loops')
+fruits = ['🍎', '🍊', '🥭', '🍉', '🍌','🍎', '🍊', '🥭', '🍉', '🍌','🍎', '🍊', '🥭', '🍉', '🍌','🍎', '🍊', '🥭', '🍉', '🍌','🍎', '🍊', '🥭', '🍉', '🍌','🍎', '🍊', '🥭', '🍉', '🍌']
+
+
+console.log(fruits)
+
+for (let i=0; i<fruits.length; i++){
+    console.log(fruits[i])
+}
+names = ['ichigo', 'sado', 'rukia', 'byakuya', 'kempachi', 'naruto']
+console.log(names)
+for (let i=0; i<names.length; i++){
+    console.log(names[i])
+}
+for (let i=0; i<names.length; i++){
+    console.log(names)
+}
+for (let i=0; i<names.length; i++){
+    console.log(names[0])
+}
+for (let i=0; i<fruits.length; i++){
+    console.log(0, fruits[0])
+}
+
+for (let i=0; i<names.length; i++){
+    console.log(1, names[1])
+}
+for (const lenn of names){
+    console.log(lenn)
+}
+powers = ['bankai', 'shikai', 'rasengan', 'chidori', 'kido']
+console.log(powers)
+for (const power of powers)
+{
+    console.log(power)
+}
+
+numbers = [1,2,3,4]
+let result=[]
+// for (number of numbers)
+// {
+//   result.push(number*2)
+// }
+// console.log(result)
+
+for (number of numbers)
+{
+  result.push(number+2)
+}
+console.log(result)
+

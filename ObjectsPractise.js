8e1b2bd0ea69227311297c913751d747f390c202
AGUSTUS <112334233+AGUSTUS01@users.noreply.github.com>
2023-02-04 17:15:02 +0530

    Add files via upload

---
+++ ObjectsPractise.js
@@ -0,0 +1,50 @@
+// powers = ['bankai', 'shikai', 'rasengan']
+// console.log(powers)
+
+
+// let x = (a, b) =>{
+//     let result = a+ " " +b;
+//     return result;
+// }
+// let ans = x('ichigo', 'kempaci')
+// console.log(ans)
+
+// let num = (a, b, c) => {
+//     let sum = a+b+c;
+//     return sum
+// }
+// let sup = num(2, 4, 6)
+// console.log(sup)
+
+// let calc = (a, b)  =>{
+// result = `this is my total money ${a} and this is my total expenses ${b} of the month`
+// return result
+// }
+// let x = calc(8500, 5500)
+// console.log(x)
+// let y = calc(3000, 1100)
+// console.log(y);
+
+// let wealth = (a, b , c) => {
+//     result = `this is the networth  of ambani ${a} this is the networth  of adani ${b} and this is the networth of elon musk ${c}`
+//     return result
+// }
+// let ans = wealth(900000, 7200000, 16500000)
+// console.log(ans)
+
+// var powers = {
+//     name1: 'ichigo', 
+//     power1:'bankai',
+//     name2: 'naruto',
+//     power2: 'rasengan',  
+//     name3: "renji",
+//     power3: "roar zabimaru",
+//     name4: "sasuke",
+//     power4: "amaterasu" ,
+//     final:  function ()  {
+//         console.log(`the most powerful is may be ${this.name1} with the help of the truth seeker balls and then 2nd place goes for ${this.name2} after that ${this.name3} kurosaki..! ichigo is not that less powewrful than ${this.name2} and ${this.name1} they may stand equally in power but.. i donno exactly how to explain. ${this.name4} abarai the lieutnent of the 6th division is the fourth powerful charecter and he is a shinigami just ike ${this.name3}`)
+//     }
+// }
+// console.log(powers.final)
+
+
